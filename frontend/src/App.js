607c190a25e2a4882dd44871f63bcef861fd2ca1
KiddoKishore <kiddokishore@gmail.com>
2023-11-28 10:38:49 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
